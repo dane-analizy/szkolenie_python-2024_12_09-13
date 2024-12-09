d6e3be5437ea7281c51980c3d3d4a0e20dd98b7c
Łukasz Prokulski <prokulski@gmail.com>
2024-12-09 10:21:15 +0100

    dzien 01

---
+++ dzien_01/main.java
@@ -0,0 +1,5 @@
+class NapisNaEkranie {
+    public static void main(String[] args) {
+        System.out.println("Ala ma kota")
+    }
+}
--- dzien_01/main.py
+++ dzien_01/main.py
@@ -1,1 +1,12 @@
-print("Ala ma kota")
+# print("Ala ma kota")
+
+# for element in lista:
+#     czynność
+
+# if coś:
+#     rób jedną rzecz
+# else:
+#     rób inną rzecz
+
+# while coś:
+#     rób te rzeczy
