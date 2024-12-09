fd9dc1621e2ee83f0fc5a25a9569b95f8c32e2b0
Łukasz Prokulski <prokulski@gmail.com>
2024-12-09 11:47:25 +0100

    dzien 01

--- dzien_01/main.py
+++ dzien_01/main.py
@@ -153,14 +153,14 @@
 # Uwaga - przy pobieraniu danych napisz w jakich jednostkach mają być podane.
 
 
-weigth = input("Podaj Twoja mase [kg]: ")
-height = input("Podaj Twoj wzrost [cm]: ")
+# weigth = input("Podaj Twoja mase [kg]: ")
+# height = input("Podaj Twoj wzrost [cm]: ")
 
-bmi = float(weigth) / (float(height) / 100) ** 2
+# bmi = float(weigth) / (float(height) / 100) ** 2
 
-print(f"Twoje BMI to: {bmi}")
+# print(f"Twoje BMI to: {bmi}")
 
-bmi_round = round(bmi, 2)
-print(f"Twoje BMI to: {bmi_round}")
+# bmi_round = round(bmi, 2)
+# print(f"Twoje BMI to: {bmi_round}")
 
-print(f"Twoje BMI to: {bmi:.3f}")
+# print(f"Twoje BMI to: {bmi:.3f}")
