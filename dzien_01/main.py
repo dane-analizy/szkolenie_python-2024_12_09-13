efd327d0ba1e1160ed98588dee055f387faf49d9
Łukasz Prokulski <prokulski@gmail.com>
2024-12-09 13:25:09 +0100

    dzien 01

--- dzien_01/main.py
+++ dzien_01/main.py
@@ -309,6 +309,10 @@
 
 # for w forze
 
-for x in range(1, 11):
-    for y in range(1, 11):
-        print(f"{x=} x {y=} = {x*y}")
+# for x in range(1, 11):
+#     for y in range(1, 11):
+#         print(f"{x=} x {y=} = {x*y}")
+
+#### ZADANIE 6
+
+# Z tabliczki mnożenia 1..10 x 1..10 wyświetl tylko te wartości, które są mniejsze niż 69.
