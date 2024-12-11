4ec2c28fa7dd1fb410d0c6d3cfcd1f737b5d4bb7
Łukasz Prokulski <prokulski@gmail.com>
2024-12-11 09:46:17 +0100

    dzien trzeci

--- dzien_03/main.py
+++ dzien_03/main.py
@@ -4,31 +4,31 @@
 # Całość posortuj po nazwisku i zapisz do pliku wyniki.csv.
 
 
-# konfiguracja
-nazwa_pliku_wejsciowego = "zawodnicy.csv"
-nazwa_pliku_wyjsciowego = "wyniki2.csv"
-separator = ";"
+# # konfiguracja
+# nazwa_pliku_wejsciowego = "zawodnicy.csv"
+# nazwa_pliku_wyjsciowego = "wyniki2.csv"
+# separator = ";"
 
-# wczytanie danych
-dane = [
-    linia.strip().split(separator)
-    for linia in open(nazwa_pliku_wejsciowego, "r", encoding="utf-8")
-]
+# # wczytanie danych
+# dane = [
+#     linia.strip().split(separator)
+#     for linia in open(nazwa_pliku_wejsciowego, "r", encoding="utf-8")
+# ]
 
-# oczyszczenie i wzbogacenie danych
-dane_wynikowe = []
-for rekord in dane:
-    waga = float(rekord[3])
-    wzrost = float(rekord[2])
-    imie = rekord[0].strip()
-    nazwisko = rekord[1].strip()
-    bmi = waga / (wzrost / 100) ** 2
+# # oczyszczenie i wzbogacenie danych
+# dane_wynikowe = []
+# for rekord in dane:
+#     waga = float(rekord[3])
+#     wzrost = float(rekord[2])
+#     imie = rekord[0].strip()
+#     nazwisko = rekord[1].strip()
+#     bmi = waga / (wzrost / 100) ** 2
 
-    dane_wynikowe.append([imie, nazwisko, wzrost, waga, bmi])
+#     dane_wynikowe.append([imie, nazwisko, wzrost, waga, bmi])
 
 
-# sortowanie listy po nazwisku
-dane_wynikowe = sorted(dane_wynikowe, key=lambda r: r[1])
+# # sortowanie listy po nazwisku
+# dane_wynikowe = sorted(dane_wynikowe, key=lambda r: r[1])
 
 # wyświetlenie na ekranie ładnych wyników
 # for r in dane_wynikowe:
@@ -63,7 +63,7 @@ dane_wynikowe = sorted(dane_wynikowe, key=lambda r: r[1])
 
 
 # wersja 2' - context manager
-dane_do_zapisania = [f"{r[0]};{r[1]};{r[2]};{r[3]};{r[4]}\n" for r in dane_wynikowe]
-# plik = open(nazwa_pliku_wyjsciowego, "w", encoding="utf-8")
-with open(nazwa_pliku_wyjsciowego, "w", encoding="utf-8") as plik:
-    plik.writelines(dane_do_zapisania)
+# dane_do_zapisania = [f"{r[0]};{r[1]};{r[2]};{r[3]};{r[4]}\n" for r in dane_wynikowe]
+# # plik = open(nazwa_pliku_wyjsciowego, "w", encoding="utf-8")
+# with open(nazwa_pliku_wyjsciowego, "w", encoding="utf-8") as plik:
+#     plik.writelines(dane_do_zapisania)
