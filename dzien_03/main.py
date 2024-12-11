6213b6fb6f2f648d800ff74df0837be01c9632d8
Łukasz Prokulski <prokulski@gmail.com>
2024-12-11 09:02:38 +0100

    dzien trzeci

--- dzien_03/main.py
+++ dzien_03/main.py
@@ -1,1 +1,4 @@
-przeliczone z pliku BMI zapisać z wynikami do nowego pliku
+#### ZADANIE 20
+
+# Wczytaj dane z pliku zawodnicy.csv, wylicz dla każdej z osób współczynnik BMI.
+# Całość posortuj po nazwisku i zapisz do pliku wyniki.csv.
