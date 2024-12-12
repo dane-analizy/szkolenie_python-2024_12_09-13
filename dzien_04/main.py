941f06f9d613553efb4da77fd3ce073b048dfc80
Łukasz Prokulski <prokulski@gmail.com>
2024-12-12 09:08:28 +0100

    dzień 4

--- dzien_04/main.py
+++ dzien_04/main.py
@@ -1,1 +1,21 @@
-dict - comprehention
+# list-comprehention
+
+# lista = [ wyrazenie(element) for element in kolekcja ]
+
+# dict comprehention
+
+# slownik = { el:el**2 for el in range(10) }
+# print(slownik)
+
+# slownik = {"kwadrat_"+str(el): el**2 for el in range(10)}
+# print(slownik)
+
+
+#### ZADANIE 25
+
+# Wczytaj konfigurację z pliku konfiguracja.txt.
+# Budowa pliku to klucz=wartość w każdej linii.
+# Wynikowo kofiguracja powinna być słownikiem.
+# Użyj dict-comprehention
+
+
