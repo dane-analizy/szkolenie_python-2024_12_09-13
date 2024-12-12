54d64713d957913a3c228b2550d395c218fbaf2b
Łukasz Prokulski <prokulski@gmail.com>
2024-12-12 09:25:02 +0100

    dzień 4

--- dzien_04/main.py
+++ dzien_04/main.py
@@ -18,4 +18,18 @@
 # Wynikowo kofiguracja powinna być słownikiem.
 # Użyj dict-comprehention
 
+# rozwiązanie zgodne z treścią zadania:
+# linie = {
+#     linia.strip().split("=")[0]: linia.strip().split("=")[1]
+#     for linia in open("konfiguracja.txt", "r", encoding="utf-8")
+# }
+# print(linie)
 
+
+# rozwiązanie czytelniejsze
+# linie = [
+#     linia.strip().split("=")
+#     for linia in open("konfiguracja.txt", "r", encoding="utf-8")
+# ]
+# konfiguracja = dict(linie)
+# print(konfiguracja)
