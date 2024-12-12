9ad568878c67bac2428c69ea1ac81f364e8e908a
Łukasz Prokulski <prokulski@gmail.com>
2024-12-12 09:53:34 +0100

    dzień 4

--- dzien_04/main.py
+++ dzien_04/main.py
@@ -47,7 +47,7 @@
 
 # for el1, el2 in zip(l1, l2):
 #     print(el1, el2)
-    
+
 
 # co z długością list? - brana jest długość najkrótszej listy
 # l1 = [1, 2, 3, 4, 5, 6]
@@ -56,16 +56,84 @@
 # for el1, el2 in zip(l1, l2):
 #     print(el1, el2)
 
-from itertools import zip_longest, cycle
+# from itertools import zip_longest, cycle
 
-l1 = [1, 2, 3, 4, 5, 6]
-l2 = ["a", "b", "c"]
+# l1 = [1, 2, 3, 4, 5, 6]
+# l2 = ["a", "b", "c"]
 
-print("zip_longest():")
-for el1, el2 in zip_longest(l1, l2):
-    print(el1, el2)
+# print("zip_longest():")
+# for el1, el2 in zip_longest(l1, l2):
+#     print(el1, el2)
 
-print("zip() + cycle():")
-for el1, el2 in zip(l1, cycle(l2)):
-    print(el1, el2)
+# print("zip() + cycle():")
+# for el1, el2 in zip(l1, cycle(l2)):
+#     print(el1, el2)
 
+
+# Zadanie 25 rozwiązanie z użyciem gotowych elementów z Pythona
+# from collections import Counter
+
+# tekst = open("pan-tadeusz.txt", "r", encoding="utf-8").read()
+# tekst = tekst.lower()
+
+# biale_znaki = ',.?!-:;…—*()%-+"«»/'
+# for bz in biale_znaki:
+#     tekst = tekst.replace(bz, " ")
+
+# wszystkie_slowa = tekst.split()
+
+# zliczenie = Counter(wszystkie_slowa)
+# zliczenie = dict(sorted(zliczenie.items(), key=lambda kv:kv[1], reverse=True))
+
+# # słowa występujące nie więcej niż 100 razy
+# for k,v in zliczenie.items():
+#     print(k,v)    
+#     if v < 100:
+#         break
+
+
+
+# wyjątki
+
+# try:
+#     spróbuj coś zrobić
+# except:
+#     zrób to, jesli nie udało się tego wyżej
+    
+# liczba = 0
+# wynik = 1/liczba
+# print(wynik)
+
+
+# liczba = 0
+# try:
+#     print("spróbuję podzielić")
+#     wynik = 1 / liczba
+#     print("podzieliłem")
+#     print("wynik:", wynik)
+# except:
+#     print("błąd")
+    
+# print("program idzie dalej")
+
+
+d = {}
+liczba = 0
+try:
+    # print(zmienna) # NameError
+    # print(d["zmiena"]) # KeyError
+    print(1 + "auagu") # TypeError
+    print("spróbuję podzielić")
+    wynik = 1 / liczba # ZeroDivisionError
+    print("podzieliłem")
+    print("wynik:", wynik)
+except NameError:
+    print("wystąpił błąd NameError")
+except TypeError:
+    print("wystąpił Type error")
+except ZeroDivisionError:
+    print("nie dziel przez zero")
+except Exception as e:
+    print("błąd", e, type(e))
+
+print("program idzie dalej")
