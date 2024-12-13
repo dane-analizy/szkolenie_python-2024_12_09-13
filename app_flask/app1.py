5526597f9508f222ac93199da4188e1de5951d0a
Łukasz Prokulski <prokulski@gmail.com>
2024-12-13 12:22:14 +0100

    dzien 5

--- app_flask/app1.py
+++ app_flask/app1.py
@@ -5,5 +5,15 @@ from flask import Flask
 
 app = Flask("nasza aplikacja")
 
+
+@app.route("/")
+def strona_glowna():
+    return "jestem stroną główną ale zmieniłem odpowiedź"
+
+
+@app.route("/kontakt")
+def kontakt():
+    return "strona z kontaktem"
+
 if __name__ == "__main__":
     app.run()
