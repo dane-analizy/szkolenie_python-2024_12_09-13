f20c00585daa3d761e6c90a6393b48a16bab6a83
Łukasz Prokulski <prokulski@gmail.com>
2024-12-13 09:26:48 +0100

    dzien 5

---
+++ nbp/utils/config.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+import yaml
+
+
+def load_config(nazwa_pliku, enc="utf-8"):
+    if not Path(nazwa_pliku).exists():
+        print(f"Plik {nazwa_pliku} nie istnieje")
+        return {}
+    with open(nazwa_pliku, "r", encoding=enc) as p:
+        config = yaml.safe_load(p)
+        return config
+    return {}
+    return {}
